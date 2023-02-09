37d90132166a353c29571c5f3ea6d608665fc189
Abid Rashid <lone.abidrashid@gmail.com>
2023-02-09 12:11:54 +0530

    update

--- plugins/db_connector.js
+++ plugins/db_connector.js
@@ -1,13 +1,24 @@
 "use strict";
 const fp = require("fastify-plugin");
+const mongoose = require("mongoose");
 const env = process.env.NODE_ENV || "DEVELOPMENT";
 const config = require("../config/config.js")[env];
 
 async function dbConnector(fastify, opts) {
-  fastify.register(require("@fastify/mongodb"), {
-    forceClose: true,
-    url: config.CONNECT_DB,
-  });
+  // // DEPRICATED
+  // // fastify.register(require("@fastify/mongodb"), {
+  //  // forceClose: true,
+  //  // url: config.CONNECT_DB,
+  // //});
+
+  //CONNECTING TO DATABASE USING MONGOOSE
+  try {
+    mongoose.set("strictQuery", false);
+    mongoose.connect(config.CONNECT_DB);
+    fastify.log.info("Database Connected");
+  } catch (e) {
+    console.error(e);
+  }
 }
 
 module.exports = fp(dbConnector);
--- routes/root.js
+++ routes/root.js
@@ -1,4 +1,16 @@
 "use strict";
+const {
+  ACCOUNT_EXISTS,
+  AUTHENTICATION_INVALID,
+  ACCOUNT_DOESNT_EXIST,
+  ACCOUNT_DETAILS_WRONG,
+  USER_ACCOUNT_DISABLED,
+  SIGN_UP_SUCCESS,
+  SERVER_ERROR,
+  RECOVER_SUCCESS,
+  DEVICE_DOESNT_EXIST,
+  AUTHENTICATION_SUCCESS,
+} = require("../config/errors.json");
 
 module.exports = async function (fastify, opts) {
   fastify.get("/", async function (request, reply) {
@@ -11,10 +23,129 @@ module.exports = async function (fastify, opts) {
   });
 
   fastify.post("/", async function (request, reply) {
-    const users = this.mongo.db.collection("users");
+    // const users = this.mongo.db.collection("users");
+    // const { name, age } = request.body;
+    // const data = { name, age };
+    // const result = await users.insertOne(data);
+    // reply.code(201).send(result);
+
+    const users = this.mongo.db.collection("new_users");
     const { name, age } = request.body;
     const data = { name, age };
     const result = await users.insertOne(data);
     reply.code(201).send(result);
   });
+
+  fastify.post(
+    "/register",
+    {
+      schema: {
+        description: "Register JWT",
+        summary: "Register JWT",
+        tags: ["JWT"],
+        body: {
+          $ref: "register_product#",
+        },
+      },
+    },
+    async function (request, reply) {
+      let language = request.headers["accept-language"]
+        ? request.headers["accept-language"]
+        : "en";
+      request.body.username = request.body.email;
+      let data = request.body;
+      let resp,
+        logs = {
+          username: request.body.email,
+          action: "Register",
+          url: "/register",
+          request_header: JSON.stringify(request.headers),
+          request: JSON.stringify(request.body),
+          axios_request: "",
+          axios_response: "",
+        };
+
+      try {
+        // GETTING USER
+        const users = fastify.mongo.db.collection("users");
+        const user = await users.findOne({
+          _id: new ObjectID(req.body.email),
+        });
+
+        // CHECKING USER IF ALREADY EXISTS
+        if (user) {
+          resp = {
+            statusCode: 400,
+            message: ACCOUNT_EXISTS[language],
+          };
+          logs.response = JSON.stringify(resp);
+          logs.status = "FAILURE";
+          await fastify.db.audit_trail.create(logs);
+          reply.code(400);
+          return resp;
+        }
+        // CHECKING USER ENDS
+
+        //ELSE -  HASHING THE PASSWORD
+        let hashedPassword = fastify.db.User.setPassword(
+          data.email,
+          data.password
+        );
+        data.password = hashedPassword;
+        // HASHING PASSWORD ENDS
+
+        // CREATING NEW USER
+        const result = await users.insertOne(data);
+
+        // Making JWT Payload
+        let payload = {
+          username: data.email,
+        };
+
+        // Generating Token
+        let token = await fastify.jwtsign(payload, data.device_id, language);
+
+        // Checking Response from JWT SIGN Plugin
+        if (token.statusCode != 202) {
+          logs.response = JSON.stringify(token);
+          logs.status = "FAILURE";
+          await fastify.db.audit_trail.create(logs);
+          reply.code(token.statusCode || 401);
+          return token;
+        }
+
+        // CREATING DEVICE DETAILS
+        let deviceDetails = {
+          device_id: data.device_id,
+          username: data.email,
+        };
+        await fastify.db.Device.create(deviceDetails);
+        // CREATING DEVICE DETAIL ENDS
+
+        reply.code(200);
+        resp = {
+          statusCode: 200,
+          message: SIGN_UP_SUCCESS[language],
+          registered_device: data.device_id,
+          access_token: token.access_token,
+          refresh_token: token.refresh_token,
+        };
+        logs.response = JSON.stringify(resp);
+        logs.status = "SUCCESS";
+        await fastify.db.audit_trail.create(logs);
+        return resp;
+      } catch (err) {
+        console.error(err);
+        resp = {
+          statusCode: 400,
+          message: SERVER_ERROR[language],
+        };
+        logs.response = JSON.stringify(resp);
+        logs.status = "FAILURE";
+        await fastify.db.audit_trail.create(logs);
+        reply.code(400);
+        return resp;
+      }
+    }
+  );
 };
