4708c0c1820478d7ec2441afbb8c1f3b3e0bb6b0
Abid Rashid <lone.abidrashid@gmail.com>
2023-02-09 13:27:30 +0530

    update

--- models/token.js
+++ models/token.js
@@ -3,11 +3,11 @@ const { Schema } = mongoose;
 
 const TokenSchema = new Schema({
   email: { type: String, required: true },
-  token_type: { type: String, required: true },
+  token_type: { type: String, enum: ["ACCESS", "REFRESH"], required: true },
   token: { type: String, required: true },
   token_expiry: { type: String, required: true },
-  token_status: { type: String, required: true },
-  device_fingerprint: { type: String, required: false },
+  token_status: { type: String, default: true },
+  device_fingerprint: { type: String, required: true },
 });
 
 const tokens = mongoose.model("tokens", TokenSchema);
