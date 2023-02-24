ad2b149614682e471678ae8dc3bc5956bc930303
Abid Rashid <lone.abidrashid@gmail.com>
2023-02-24 15:13:14 +0530

    update

--- models/question_response.js
+++ models/question_response.js
@@ -1,9 +1,12 @@
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
 
-const responseSchema = new Schema({
-  response: { type: Object, required: true },
-});
+const responseSchema = new Schema(
+  {
+    response: { type: Object, required: true },
+  },
+  { timestamps: true }
+);
 
 const question_response = mongoose.model("question_response", responseSchema);
 
